e1d9b6ed5aa3da1adfeae57249f367463ca69bb3
Frank Hillberg <Frank.Hillberg@lexisnexisrisk.com>
2022-06-10 15:40:08 -0400

    make it work if db is down

--- Lightman/Lightman.Mvc/Services/AirportService.cs
+++ Lightman/Lightman.Mvc/Services/AirportService.cs
@@ -9,13 +9,20 @@
         }
         public string LookupAirportDescription(string code)
         {
-            var airport = _dbContext.Airports.FirstOrDefault(x => x.Ident == code);
-            if (airport == null)
+            try
             {
-                return "Not Found";
-            }
+                var airport = _dbContext.Airports.FirstOrDefault(x => x.Ident == code);
+                if (airport == null)
+                {
+                    return $"{code} (Airport description not found)";
+                }
 
-            return $"{airport.Ident} {airport.AirportName ?? ""}, {airport.Municipality}, {airport.IsoRegion} ({airport.AirportType ?? ""})";
+                return $"{airport.Ident} {airport.AirportName ?? ""}, {airport.Municipality}, {airport.IsoRegion} ({airport.AirportType ?? ""})";
+            }
+            catch (Exception ex)
+            {
+                return $"{code} (Airport description not found)";
+            }
         }
     }
 }
